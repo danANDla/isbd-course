999883b43de535e6c5869271b53dafabb5d02539
archdla <d.a.n.202021@gmail.com>
2022-12-22 17:47:42 +0300

    add trigger for checking ingredients for cocktail

--- triggers.sql
+++ triggers.sql
@@ -3,15 +3,78 @@ create or replace function check_cocktail_in_menu() returns trigger as $$
 begin
     if new.cocktail_id not in (select cocktail_id from menus where party_id = new.party_id)
     then
-        RAISE EXCEPTION 'cocktail is not in menus of this party';
+        RAISE EXCEPTION 'cocktail is not in the menu of this party';
     end if;
     return new;
 end;
 $$ language plpgsql;
-create or replace trigger check_cocktail_in_menu_trigger
+create trigger a_check_cocktail_in_menu_trigger
     before insert or update on orders
     for each row
 execute procedure check_cocktail_in_menu();
+
+/*проерка наличия ингредиентов для коктейля*/
+create or replace function check_ingredients_in_purchase() returns trigger as $$
+declare
+    mview record;
+    remain real;
+    new_quantity real;
+    result integer;
+    price real;
+    c_name text;
+begin
+    result := 1;
+
+    for mview in 
+        select cocktails.name as cocktail, ingredients.name as ingredient, recipes.quantity as quantity, ingredients.id as ingr_id from ingredients
+            JOIN recipes on recipes.ingredient_id = ingredients.id
+            JOIN cocktails on cocktails.id = recipes.cocktail_id
+        where cocktails.id = new.cocktail_id 
+    loop
+        if mview.ingr_id in(
+            select ingredients.id from ingredients
+                join products on products.ingredient_id = ingredients.id
+                join purchases on purchases.product_id = products.id
+            where purchases.party_id = new.party_id
+        )
+        then
+            select purchases.quantity from ingredients
+                join products on products.ingredient_id = ingredients.id
+                join purchases on purchases.product_id = products.id
+            where purchases.party_id = new.party_id and products.ingredient_id = mview.ingr_id order by purchases.quantity desc limit 1 into remain;
+
+            new_quantity:= remain - mview.quantity;
+            if new_quantity < 0
+            then
+                RAISE NOTICE '[%] ~ % %',
+                             mview.cocktail,
+                             mview.ingredient,
+                             mview.quantity;
+                result := 0;
+            end if;
+        else
+            RAISE NOTICE '[%] ☓ % %',
+                         mview.cocktail,
+                         mview.ingredient,
+                         mview.quantity;
+            result := 0;
+        end if;
+    end loop;  
+    
+    if result = 0
+    then
+        RAISE EXCEPTION 'not enough ingredients for this cocktail';
+    end if;
+
+    return new;
+end;
+$$ language plpgsql;
+create trigger b_check_ingredients_in_purchase_trigger
+    before insert or update on orders
+    for each row
+execute procedure check_ingredients_in_purchase();
+
+
  
 /* проверка приглашен ли пользователь */
 create or replace function check_user_invited() returns trigger as $$
