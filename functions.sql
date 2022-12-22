0a1d2d43905b6e00080a765dcc471d3b3eb4dbc3
archdla <d.a.n.202021@gmail.com>
2022-12-22 16:14:28 +0300

    ordering cocktail in case of several products of same ingredients

--- functions.sql
+++ functions.sql
@@ -1,7 +1,5 @@
 /*Проверка можно ли добавить коктейль в заказ*/
-select * from add_item_to_order(1,2,1);
-
-create or replace function add_item_to_order(p_id bigint, c_id bigint, u_id bigint) returns integer as $$
+create or replace procedure add_item_to_order(p_id bigint, c_id bigint, u_id bigint) as $$
 declare
     mview record;
     remain real;
@@ -84,13 +82,13 @@ begin
         execute format('insert into orders(name,price,party_id,person_id,cocktail_id)
                 values($1,$2,$3,$4,$5);') using c_name,price,p_id,u_id,c_id; 
     end if;
-    return result;
+    -- return result;
 end;
 $$ language plpgsql;
 
 
 /*Добавить продукт в purchases*/
-create or replace function add_product_to_purchase(p_id bigint, prod_id bigint, quantity real) returns integer as $$
+create or replace procedure add_product_to_purchase(p_id bigint, prod_id bigint, quantity real) as $$
 declare
     remain real;
     new_quantity real;
@@ -101,33 +99,32 @@ begin
     )
     then
         RAISE NOTICE 'invalid product id';
-        return 0;
-    end if;
+    else
     
-    if prod_id not in(
-        select id from products
-    )
-    then
-        execute format('insert into purchases(party_id,product_id,quantity)
-                values($1,$2,$3);') using p_id, prod_id, 0; 
+        if prod_id not in(
+            select id from products
+        )
+        then
+            execute format('insert into purchases(party_id,product_id,quantity)
+                    values($1,$2,$3);') using p_id, prod_id, 0; 
+        end if;
+
+        select purchases.quantity from ingredients
+            join products on products.ingredient_id = ingredients.id
+            join purchases on purchases.product_id = products.id
+        where purchases.party_id = p_id and products.id = prod_id into remain;
+
+        new_quantity := remain + quantity;
+        if new_quantity < 0
+        then
+            RAISE NOTICE 'new value of quantity can not be negative:';
+            RAISE NOTICE 'product[%] % + (%) = %',
+            prod_id, remain, quantity, new_quantity;
+        else
+            execute format('update purchases set quantity = $2 where product_id = $1 and party_id = $3;')
+                using prod_id, new_quantity, p_id; 
+        end if;
+
     end if;
-
-    select purchases.quantity from ingredients
-        join products on products.ingredient_id = ingredients.id
-        join purchases on purchases.product_id = products.id
-    where purchases.party_id = p_id and products.id = prod_id into remain;
-
-    new_quantity := remain + quantity;
-    if new_quantity < 0
-    then
-        RAISE NOTICE 'new value of quantity can not be negative:';
-        RAISE NOTICE 'product[%] % + (%) = %',
-        prod_id, remain, quantity, new_quantity;
-        return 0;
-    end if;
-
-    execute format('update purchases set quantity = $2 where product_id = $1 and party_id = $3;')
-        using prod_id, new_quantity, p_id; 
-    return 1;
 end;
 $$ language plpgsql;
