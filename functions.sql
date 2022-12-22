1f0a382aa749a2af50c3db034fa87984e5ca7578
archdla <d.a.n.202021@gmail.com>
2022-12-22 17:48:10 +0300

    removed checking from adding to cocktail

--- functions.sql
+++ functions.sql
@@ -8,80 +8,43 @@ declare
     price real;
     c_name text;
     prod record;
+    order_id bigint;
 begin
-    result := 1;
-    price := 0;
+    execute format('insert into orders(name,price,party_id,person_id,cocktail_id)
+            values($1,$2,$3,$4,$5);') using c_name, 0, p_id,u_id,c_id; 
+    SELECT LAST_INSERT_ID() into order_id;
 
+    select cocktails.name from cocktails where cocktails.id = c_id into c_name;
     for mview in 
         select cocktails.name as cocktail, ingredients.name as ingredient, recipes.quantity as quantity, ingredients.id as ingr_id from ingredients
             JOIN recipes on recipes.ingredient_id = ingredients.id
             JOIN cocktails on cocktails.id = recipes.cocktail_id
         where cocktails.id = c_id 
     loop
-        
-        if mview.ingr_id in(
-            select ingredients.id from ingredients
-                join products on products.ingredient_id = ingredients.id
-                join purchases on purchases.product_id = products.id
-            where purchases.party_id = p_id
-        )
-        then
-            select purchases.quantity from ingredients
-                join products on products.ingredient_id = ingredients.id
-                join purchases on purchases.product_id = products.id
-            where purchases.party_id = p_id into remain;
+        select purchases.quantity from ingredients
+            join products on products.ingredient_id = ingredients.id
+            join purchases on purchases.product_id = products.id
+        where purchases.party_id = p_id and ingredients.id = mview.ingr_id order by purchases.quantity desc limit 1 into remain;
+        new_quantity:= remain - mview.quantity;
+        RAISE NOTICE '[%] ✓ % %, will remain -> %',
+                     mview.cocktail,
+                     mview.ingredient,
+                     mview.quantity,
+                     new_quantity;
 
-            new_quantity:= remain - mview.quantity;
-            if new_quantity < 0
-            then
-                RAISE NOTICE '[%] ~ % %',
-                             mview.cocktail,
-                             mview.ingredient,
-                             mview.quantity;
-                result := 0;
-            end if;
-        else
-            RAISE NOTICE '[%] ☓ % %',
-                         mview.cocktail,
-                         mview.ingredient,
-                         mview.quantity;
-            result := 0;
-        end if;
+        select products.id as id, products.price as price from ingredients
+            join products on products.ingredient_id = ingredients.id
+            join purchases on purchases.product_id = products.id
+        where purchases.party_id = p_id and ingredients.id = mview.ingr_id order by purchases.quantity desc limit 1 into prod;
+
+        execute format('update purchases set quantity = $2 where product_id = $1 and party_id = $3;')
+            using prod.id, new_quantity, p_id; 
+
+        price := price + (mview.quantity * prod.price);
     end loop;  
-    if result = 1
-    then
-        select cocktails.name from cocktails where cocktails.id = c_id into c_name;
-        for mview in 
-            select cocktails.name as cocktail, ingredients.name as ingredient, recipes.quantity as quantity, ingredients.id as ingr_id from ingredients
-                JOIN recipes on recipes.ingredient_id = ingredients.id
-                JOIN cocktails on cocktails.id = recipes.cocktail_id
-            where cocktails.id = c_id 
-        loop
-            select purchases.quantity from ingredients
-                join products on products.ingredient_id = ingredients.id
-                join purchases on purchases.product_id = products.id
-            where purchases.party_id = p_id and ingredients.id = mview.ingr_id into remain;
-            new_quantity:= remain - mview.quantity;
-            RAISE NOTICE '[%] ✓ % %, will remain -> %',
-                         mview.cocktail,
-                         mview.ingredient,
-                         mview.quantity,
-                         new_quantity;
-
-            select products.id as id, products.price as price from ingredients
-                join products on products.ingredient_id = ingredients.id
-                join purchases on purchases.product_id = products.id
-            where purchases.party_id = p_id and ingredients.id = mview.ingr_id into prod;
-
-            execute format('update purchases set quantity = $2 where product_id = $1 and party_id = $3;')
-                using prod.id, new_quantity, p_id; 
-
-            price := price + (mview.quantity * prod.price);
-        end loop;  
-        RAISE NOTICE 'price = %', price;
-        execute format('insert into orders(name,price,party_id,person_id,cocktail_id)
-                values($1,$2,$3,$4,$5);') using c_name,price,p_id,u_id,c_id; 
-    end if;
+    RAISE NOTICE 'price = %', price;
+    execute format('update orders set price = $2 where orders.id = $1;')
+        using order_id, price; 
     -- return result;
 end;
 $$ language plpgsql;
@@ -102,7 +65,7 @@ begin
     else
     
         if prod_id not in(
-            select id from products
+            select product_id from purchases where purchases.party_id = p_id
         )
         then
             execute format('insert into purchases(party_id,product_id,quantity)
